8f349087510aea3d1b6002032c46446492e9f99e
JoseJuarez.Junior <juarez.junior@outlook.com>
2022-11-01 08:15:53 -0300

    Adding SQLite, Prisma and creating migration

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,13 @@ async function bootstrap() {
     const fastify = Fastify ({
         logger: true,
     })
+
+    //http://localhost:3333
+    fastify.get('/pools/count', () => {
+        return { count: 0 }
+    })
+
+    await fastify.listen({ port: 3333 })
 }
 
 bootstrap();
